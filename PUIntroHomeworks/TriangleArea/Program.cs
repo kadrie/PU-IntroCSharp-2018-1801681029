2c6436421eed89d3c6943d27b7627e11f545b1a8
kadrie <kadrie_989@abv.bg>
2018-11-25 18:26:43 +0000

    homework

---
+++ PUIntroHomeworks/TriangleArea/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TriangleArea
+{
+    class Program
+    {
+        static Random rand = new Random();
+        static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            
+            double triagnleHeight = rand.Next(33, 188);
+            double triangleBase = rnd.Next(33, 10000);
+            double triangleArea = ((0.5*triangleBase)*triagnleHeight);
+
+            Console.WriteLine("The triangle's random height is: " + triagnleHeight);
+            Console.WriteLine("The triangle's random base is: " + triangleBase);
+            Console.WriteLine("The triangle's area is: {0:F2}", triangleArea);
+        }
+    }
+}
